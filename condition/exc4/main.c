2b730f5788841cec2fc3715fff3a0f7307501ab3
younes-oss <rihabsabri21@gmail.com>
2025-09-23 17:00:30 +0100

    conditionl2

--- condition/exc4/main.c
+++ condition/exc4/main.c
@@ -4,8 +4,9 @@
 
 int main()
 {
-    int a,b,c ;
-    float delta ,s1,s2 , s0;
+    int a, b, c;
+    float delta, s1, s2, s0;
+
     printf("entre la valeur de a : ");
     scanf("%d", &a);
     printf("entre la valeur de b : ");
@@ -13,21 +14,20 @@ int main()
     printf("entre la valeur de c : ");
     scanf("%d", &c);
 
-    delta = (b*b) - 4*a*c;
+    delta = (b * b) - 4 * a * c;
 
-    s1 = -b-sqrt(delta)/2*a;
-    s2 = -b+sqrt(delta)/2*a;
-    s0 = -b / 2*a;
-
-    if(delta>0){
-        printf("vous avez deux solutions :\n s1 : %d \n s2 : %d : " ,s1,s2);
+    if (delta > 0) {
+        s1 = (-b - sqrt(delta)) / (2 * a);
+        s2 = (-b + sqrt(delta)) / (2 * a);
+        printf("Vous avez deux solutions :\n s1 = %f\n s2 = %f\n", s1, s2);
     }
-    else if(delta = 0)
+    else if (delta == 0) {
+        s0 = (-b) / (2.0 * a);
+        printf("Vous avez une seule solution : %f\n", s0);
+    }
+    else {
+        printf("L'équation n'admet pas de solution réelle.\n");
+    }
 
-        printf( "vous avez une seul solution : %d",s0);
-
-        else
-            printf("alors l'équation n'admet pas de solution réelle");
-
-
+    return 0;
 }
